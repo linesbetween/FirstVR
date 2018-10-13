9319d7ee1b70e52421b7f783141c404e48305518
linesbetween <14285266+linesbetween@users.noreply.github.com>
2018-10-13 03:14:09 -0400

    initial

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int speed = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// update player position based on input
+		Vector3 position = transform.position;
+		position.x += moveHorizontal * speed * Time.deltaTime;
+		position.z += moveVertical * speed * Time.deltaTime;
+		transform.position = position;
+	}
+}
